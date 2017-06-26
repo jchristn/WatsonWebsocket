a04551c62d9e5bba49204867d8f806a7cd845c6c
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2017-06-26 17:32:37 +0100

    clean up references

--- WatsonWebsocket/ClientMetadata.cs
+++ WatsonWebsocket/ClientMetadata.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Net.Security;
-using System.Net.Sockets;
 using System.Net.WebSockets;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace WatsonWebsocket
 {
