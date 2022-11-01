fc4ee67e11e929f84f8457f327a6cf85c57c87f2
zekiahepic <73035340+Zekiah-A@users.noreply.github.com>
2022-11-01 23:29:50 +0000

    Update TestDebug.sh

--- TestDebug.sh
+++ TestDebug.sh
@@ -1,10 +1,1 @@
-$project_dir=$(pwd)
-
-cd src/Test.Server/bin/debug/net6.0
-./Test.Server
-TIMEOUT 1 > NUL
-cd $project_dir
-
-cd src/Test.Client/bin/debug/net6.0
-./Test.Client
-cd $project_dir
+./src/Test.Server/bin/debug/net6.0/Test.Server & ./src/Test.Client/bin/debug/net6.0/Test.Client && fg
